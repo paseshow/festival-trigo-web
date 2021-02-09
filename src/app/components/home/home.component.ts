b37ad01c773c3802a0aad0bed61172483fbf1ad6
nairalucila <nairacoppari@gmail.com>
2021-02-09 12:29:21 -0300

    sponsors agregados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,11 +12,13 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  slides = ["BolsaCerealesBahiaB", "BolsaCerealescordoba" , "BolsadeCereales", "Clubleones", "Cordobaentretodos", "MinisterioAgriculturaganaderia", "Sociedadruraleones"];
+  // slides = ["BolsaCerealesBahiaB", "BolsaCerealescordoba" , "BolsadeCereales", "Clubleones", "Cordobaentretodos", "MinisterioAgriculturaganaderia", "Sociedadruraleones"];
+
+  slides = [{nombre: "alcance", tamano: "100"},{nombre: "asoCoperatArg", tamano: "100"}, {nombre: "bancor", tamano: "100"}, {nombre: "bertoto", tamano: "100"}, {nombre: "capello", tamano: "100"}, {nombre: "contimix", tamano: "100"}, {nombre: "contingiani", tamano: "100"}, {nombre: "coperativaleones", tamano: "100"}, {nombre: "gastaldi", tamano: "100"}, {nombre: "leones2", tamano: "100"}, {nombre: "metalfor", tamano: "100"}, {nombre: "ministerioAgGan", tamano: "100"}, {nombre: "salas", tamano: "100"}, {nombre: "torregiani", tamano: "100"}, {nombre: "union100", tamano: "100"}];
 
 
   slideConfig = {
-    "slidesToShow": 7,
+    "slidesToShow": 6,
     "slidesToScroll": 1,
     "nextArrow": "<div class='nav-btn next-slide'></div>",
     "prevArrow": "<div class='nav-btn prev-slide'></div>",
@@ -24,29 +26,29 @@ export class HomeComponent implements OnInit {
     "infinite": false
   };
 
-  addSlide() {
-    this.slides.push()
-  }
+  // addSlide() {
+  //   this.slides.push()
+  // }
 
-  removeSlide() {
-    this.slides.length = this.slides.length - 1;
-  }
+  // removeSlide() {
+  //   this.slides.length = this.slides.length - 1;
+  // }
 
-  slickInit(e) {
-    console.log('slick initialized');
-  }
+  // slickInit(e) {
+  //   console.log('slick initialized');
+  // }
 
-  breakpoint(e) {
-    console.log('breakpoint');
-  }
+  // breakpoint(e) {
+  //   console.log('breakpoint');
+  // }
 
-  afterChange(e) {
-    console.log('afterChange');
-  }
+  // afterChange(e) {
+  //   console.log('afterChange');
+  // }
 
-  beforeChange(e) {
-    console.log('beforeChange');
-  }
+  // beforeChange(e) {
+  //   console.log('beforeChange');
+  // }
 
   
 }
