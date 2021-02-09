3879b598ea92c9f625eeb753e5f4c80819140284
nairalucila <nairacoppari@gmail.com>
2021-02-09 11:22:56 -0300

    carrusel de logos en proceso

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { ComponentRoutes } from './components.routing';
-
 // COMPONENTES ---------------------------------------------
 import { HomeComponent } from './home/home.component';
 import { StreamingComponent } from './streaming/streaming.component';
+
 
 
 @NgModule({
     imports: [
         RouterModule.forChild(ComponentRoutes),
         CommonModule,
+        SlickCarouselModule,
     ],
     exports: [],
     declarations: [
